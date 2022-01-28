d5741c7ba1a737ca480927b4302cb1fddd050d69
toleksa <toleksa@gmail.com>
2022-01-28 18:06:29 +0000

    pass generator fix

--- kube/deploy.sh
+++ kube/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-head /dev/urandom | tr -dc A-Za-z0-9 | head -c 12 ; echo '' | base64 | kubectl create secret generic mariadb-secret --from-literal=mariadb-root-password=-
+kubectl create secret generic mariadb-secret --from-literal=password=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 12 | base64)
 kubectl apply -f mariadb.yaml
 kubectl apply -f python-rest-api.yaml
