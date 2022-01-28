9c739d59adef7c830be7587a1d4eb2fc750e7bc3
toleksa <toleksa@gmail.com>
2022-01-28 13:28:13 +0100

    first

--- kube/run.sh
+++ kube/run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-head /dev/urandom | tr -dc A-Za-z0-9 | head -c 12 ; echo '' | base64 | kubectl create secret generic mariadb-root-password --from-literal=password=-
-
+head /dev/urandom | tr -dc A-Za-z0-9 | head -c 12 ; echo '' | base64 | kubectl create secret generic mariadb-secret --from-literal=mariadb-root-password=-
