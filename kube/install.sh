e6be9c22987fc59bbfef543a4ec199d7399c33df
toleksa <toleksa@gmail.com>
2022-02-02 12:59:39 +0100

    mariadb init

--- kube/install.sh
+++ kube/install.sh
@@ -26,6 +26,7 @@ METALLB_ADDRESSES=${METALLB_ADDRESSES:=`hostname -I | awk '{print $1"-"$1}'`} en
 
 echo "deploying app"
 kubectl create secret generic mariadb --from-literal=mariadb-password=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 12 | base64) --from-literal=mariadb-root-password=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 12 | base64)
+kubectl create configmap mariadb-init --from-literal=init.sql="create table python_rest_api.dict(k varchar(50) primary key,v varchar (50)); insert into python_rest_api.dict (k,v) values ('Homer','Simpson'); insert into python_rest_api.dict (k,v) values ('Jeffrey','Lebowski'); insert into python_rest_api.dict (k,v) values ('Stan','Smith');"
 kubectl apply -f mariadb.yaml
 kubectl apply -f python-rest-api.yaml
 
