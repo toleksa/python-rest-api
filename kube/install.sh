266e0f751fc349adc00762781ae8a33a25e92ebd
toleksa <toleksa@gmail.com>
2022-02-02 13:27:09 +0100

    kube ready (perhaps)

--- kube/install.sh
+++ kube/install.sh
@@ -17,12 +17,26 @@ else
 fi
 
 echo "installing longhorn"
-./install-longhorn.sh
+kubectl create namespace longhorn-system
+kubectl apply -f https://raw.githubusercontent.com/longhorn/longhorn/master/deploy/longhorn.yaml
 
 echo "installing metallb"
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/main/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/main/manifests/metallb.yaml
-METALLB_ADDRESSES=${METALLB_ADDRESSES:=`hostname -I | awk '{print $1"-"$1}'`} envsubst < metallb-configmap.yaml | kubectl apply -f -
+kubectl apply -f - <<EOF
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  namespace: metallb-system
+  name: config
+data:
+  config: |
+    address-pools:
+    - name: default
+      protocol: layer2
+      addresses:
+      - `hostname -I | awk '{print $1"-"$1}'`
+EOF
 
 echo "deploying app"
 kubectl create secret generic mariadb --from-literal=mariadb-password=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 12 | base64) --from-literal=mariadb-root-password=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 12 | base64)
