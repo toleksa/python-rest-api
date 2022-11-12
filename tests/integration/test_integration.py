f43633898108b0faff25b1d7f41d70c6029d0a55
toleksa <toleksa@gmail.com>
2022-11-12 19:30:36 +0200

    integration test - better test

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -1,11 +1,21 @@
-# yeah, those aren't proper unit tests, just had to put anything here
-import os
+import requests
 
-def test_app():
-    assert os.path.exists("app")
-    assert os.path.isdir("app")
+# methods in requests object:
+# https://www.w3schools.com/python/ref_requests_response.asp
 
-def test_index():
-    os.path.exists("app/server.py")
-    assert os.path.getsize("app/server.py") == 1225
+def test_server_is_alive():
+    response = requests.get("http://webserver/health")
+    assert response.status_code == 200
+
+def test_server_logo():
+    response = requests.get("http://webserver/logo.png")
+    assert response.status_code == 200
+    assert len(response.content) == 11543
+    assert response.ok
+    assert response.is_redirect == False
+
+def test_server_404():
+    response = requests.get("http://webserver/foobar")
+    assert response.status_code == 404
+
 
