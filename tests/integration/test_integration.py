eff812951a64094242d4f1adec3dc51811b25719
toleksa <toleksa@gmail.com>
2022-11-13 20:01:29 +0200

    proper port

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -4,18 +4,18 @@ import requests
 # https://www.w3schools.com/python/ref_requests_response.asp
 
 def test_server_is_alive():
-    response = requests.get("http://webserver/health")
+    response = requests.get("http://webserver:5000/health")
     assert response.status_code == 200
 
 def test_server_logo():
-    response = requests.get("http://webserver/logo.png")
+    response = requests.get("http://webserver:5000/logo.png")
     assert response.status_code == 200
     assert len(response.content) == 11543
     assert response.ok
     assert response.is_redirect == False
 
 def test_server_404():
-    response = requests.get("http://webserver/foobar")
+    response = requests.get("http://webserver:5000/foobar")
     assert response.status_code == 404
 
 
