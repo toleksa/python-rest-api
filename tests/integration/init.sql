3548bc538468cddd0588c3d1ce9ff0682fdc2b47
toleksa <toleksa@gmail.com>
2022-11-17 00:52:22 +0200

    use init.sql from app/

--- tests/integration/init.sql
+++
@@ -1,4 +0,0 @@
-create table python_rest_api.dict(k varchar(50) primary key,v varchar (50));
-insert into python_rest_api.dict (k,v) values ('Homer','Simpson');
-insert into python_rest_api.dict (k,v) values ('Jeffrey','Lebowski');
-insert into python_rest_api.dict (k,v) values ('Stan','Smith');
