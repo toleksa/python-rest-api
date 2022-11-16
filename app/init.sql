bdd32159150378c8c98293fe01051f42ccd0a257
toleksa <toleksa@gmail.com>
2022-11-17 00:30:01 +0200

    init

---
+++ app/init.sql
@@ -0,0 +1,4 @@
+create table python_rest_api.dict(k varchar(50) primary key,v varchar (50));
+insert into python_rest_api.dict (k,v) values ('Homer','Simpson');
+insert into python_rest_api.dict (k,v) values ('Jeffrey','Lebowski');
+insert into python_rest_api.dict (k,v) values ('Stan','Smith');
