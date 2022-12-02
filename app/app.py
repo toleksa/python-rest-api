5f17d0fe7660fae3d098d57ab312b707bff4084b
toleksa <toleksa@gmail.com>
2022-12-02 02:57:30 +0200

    formatting - whitespaces

--- app/app.py
+++ app/app.py
@@ -38,48 +38,45 @@ def go_to_data():
 
 @app.route('/cache', methods=['GET'])
 def select_cache():
-  res = []
-  keys = red.keys()
-  for key in keys:
-    value = res.append((key.decode("utf-8"), red.get(key.decode("utf-8")).decode("utf-8")))
-  return res
+    res = []
+    keys = red.keys()
+    for key in keys:
+        value = res.append((key.decode("utf-8"), red.get(key.decode("utf-8")).decode("utf-8")))
+    return res
 
 def select_all():
-  cur = conn.cursor()
-  query = "SELECT * from dict"
-  k =request.args.get('k')
-  if k is not None:
-    query += " WHERE k='" + str(request.args.get('k')) +"'"
-  cur.execute(query)
-  res = []
-  for (k, v) in cur:
-    res.append((k,v))
-  return res
+    cur = conn.cursor()
+    query = "SELECT * from dict"
+    k =request.args.get('k')
+    if k is not None:
+        query += " WHERE k='" + str(request.args.get('k')) +"'"
+    cur.execute(query)
+    res = []
+    for (k, v) in cur:
+        res.append((k,v))
+    return res
 
 @app.route('/data', methods=['GET'])
 def select():
-  res = []
+    res = []
 
-  key = request.args.get("key")
-  if key is None:
-    res=select_all()
-  else:
-    value = red.get(key)
-    if value is None:
-      query = f'SELECT v FROM dict WHERE k="{key}"' 
-      cur = conn.cursor()
-      cur.execute(query)
-      result = cur.fetchone()
-      print(result)
-      if result is not None:
-        value = result[0]
-        red.set(key,value)
+    key = request.args.get("key")
+    if key is None:
+        res=select_all()
     else:
-      value = value.decode("utf-8")
-    res.append((key, value))
-
-  print(res)
-  return jsonify(res)
+        value = red.get(key)
+        if value is None:
+            query = f'SELECT v FROM dict WHERE k="{key}"' 
+            cur = conn.cursor()
+            cur.execute(query)
+            result = cur.fetchone()
+            if result is not None:
+                value = result[0]
+                red.set(key,value)
+        else:
+            value = value.decode("utf-8")
+        res.append((key, value))
+    return jsonify(res)
 
 @app.route('/data', methods=['POST'])
 def insert():
@@ -92,6 +89,7 @@ def insert():
     except mariadb.Error as e:
       print(f"Error: {e}")
     conn.commit()
+    #TODO: add to redis
   return '', 204
 
 @app.route('/health')
