989f47c812a536e460945c017fefe4d0406bb875
toleksa <toleksa@gmail.com>
2023-01-05 02:12:36 +0200

    DictionaryTable.js - revert - get API_URL from env variables

--- wui/src/DictionaryTable.js
+++ wui/src/DictionaryTable.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const API_URL = 'http://192.168.0.136:5000';
-//const API_URL = process.env.API_URL;
 
 function DictionaryTable() {
   const [data, setData] = useState([]);
