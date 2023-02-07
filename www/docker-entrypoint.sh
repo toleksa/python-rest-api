8951c5682d78ca63c0af74835a90483ca4189d87
toleksa <toleksa@gmail.com>
2023-02-07 18:43:01 +0200

    WWW port variable

--- www/docker-entrypoint.sh
+++ www/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -eu
 
-envsubst '${ENV_API_URL}' < /etc/nginx/conf.d/default.template > /etc/nginx/conf.d/default.conf
+envsubst '${ENV_API_URL} ${WWW_PORT}' < /etc/nginx/conf.d/default.template > /etc/nginx/conf.d/default.conf
 
 exec "$@"
 
