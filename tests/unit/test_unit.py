08c6ff00c47690437628ce279b32ca166b69cdeb
toleksa <toleksa@gmail.com>
2023-02-07 19:31:20 +0200

    WWW add tests - unit

--- tests/unit/test_unit.py
+++ tests/unit/test_unit.py
@@ -5,8 +5,16 @@ def test_app():
     assert os.path.exists("app")
     assert os.path.isdir("app")
 
-def test_index():
+def test_app_index():
     os.path.exists("app/app.py")
     #TODO: restore it after finish working on app.py
     #assert os.path.getsize("app/app.py") == 2693
+
+def test_www():
+    assert os.path.exists("www")
+    assert os.path.isdir("www")
+
+def test_www_index():
+    os.path.exists("www/index.html")
+    os.path.exists("www/python-rest-api.js")
 
