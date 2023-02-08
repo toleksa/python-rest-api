5e2fb0a7133f20acfa97b0d8b02bbc464136550a
toleksa <toleksa@gmail.com>
2023-02-08 20:19:43 +0200

    🗑️

--- docker/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd ..
-docker build -t python-rest-api:latest .
-
--- docker/push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#docker login docker.io / toleksa
-
-docker tag python-rest-api:latest toleksa/python-rest-api:latest
-docker push toleksa/python-rest-api:latest
-
