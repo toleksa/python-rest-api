ba9286105339dbcac777d4c6078de7cd2de5242e
toleksa <toleksa@gmail.com>
2024-02-15 11:01:48 +0100

    app.py - linter fixes 2

--- app/app.py
+++ app/app.py
@@ -19,7 +19,6 @@ from utils import setting_statsd, StatsdMiddleware
 from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.sdk.trace.export import ConsoleSpanExporter
 from opentelemetry.exporter.jaeger.thrift import JaegerExporter
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
